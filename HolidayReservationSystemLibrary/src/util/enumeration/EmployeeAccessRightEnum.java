5a2f23758662803828c01f168b43c0940988331c
Er Ming Yong <mingyong1999@gmail.com>
2021-10-21 16:21:09 +0800

    Added Enumeration and Exceptions

---
+++ HolidayReservationSystemLibrary/src/util/enumeration/EmployeeAccessRightEnum.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.enumeration;
+
+/**
+ *
+ * @author mingy
+ */
+public enum EmployeeAccessRightEnum {
+    SYSTEM_ADMINISTRATOR,
+    GUEST_RELATION_OFFICER,
+    OPERATION_MANAGER,
+    SALES_MANAGER
+    
+}
